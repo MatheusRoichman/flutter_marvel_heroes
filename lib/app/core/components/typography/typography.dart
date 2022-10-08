238818b60fdd639bb2c05945b49048dc6125f33e
Matheus Roichman <roichmankill@gmail.com>
2022-10-08 18:49:04 -0300

    added TTypography class, that contains the TextStyle's for each style specified on the project's style guide

---
+++ lib/app/core/components/typography/typography.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../../utils/constants.dart';
+
+abstract class TTypography {
+  static const profileTitle = TextStyle(
+    fontSize: 40,
+    fontWeight: FWeight.extraBold,
+  );
+
+  static const homeTitle = TextStyle(
+    fontSize: 32,
+    fontWeight: FWeight.extraBold,
+  );
+
+  static const cardTitle = TextStyle(
+    fontSize: 20,
+    fontWeight: FWeight.extraBold,
+  );
+
+  static const sectionTitle = TextStyle(
+    fontSize: 18,
+    fontWeight: FWeight.bold,
+  );
+
+  static const profileSubtitle = TextStyle(
+    fontSize: 16,
+    fontWeight: FWeight.medium,
+  );
+
+  static const homeSubtitle = TextStyle(
+    fontSize: 14,
+    fontWeight: FWeight.semiBold,
+  );
+
+  static const cardSubtitle = TextStyle(
+    fontSize: 10,
+    fontWeight: FWeight.medium,
+  );
+
+  static const description = TextStyle(
+    fontSize: 14,
+    fontWeight: FWeight.medium,
+  );
+
+  static const caracteristic = TextStyle(
+    fontSize: 12,
+    fontWeight: FWeight.medium,
+  );
+
+  static const ability = TextStyle(
+    fontSize: 12,
+    fontWeight: FWeight.regular,
+  );
+}
