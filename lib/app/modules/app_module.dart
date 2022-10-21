0fa41903911c845d7f8fc187efaba7f6d0c625bb
Matheus Roichman <roichmankill@gmail.com>
2022-10-20 21:01:04 -0300

    added EntryModule

--- lib/app/modules/app_module.dart
+++ lib/app/modules/app_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'home/home_module.dart';
+import 'entry/entry_module.dart';
 
 class AppModule extends Module {
   @override
@@ -8,6 +8,6 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: EntryModule()),
   ];
 }
