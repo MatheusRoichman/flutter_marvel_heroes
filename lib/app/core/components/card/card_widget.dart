d0af0d9c5388361ca40baac8c8420025b9e903ee
Matheus Roichman <roichmankill@gmail.com>
2022-10-20 21:04:20 -0300

    title and subtile are no more required in CardWidget; added fields 'onTap' and 'gradient' to CardWidget

--- lib/app/core/components/card/card_widget.dart
+++ lib/app/core/components/card/card_widget.dart
@@ -5,58 +5,65 @@ import '../../utils/constants.dart';
 import '../typography/typography.dart';
 
 class CardWidget extends StatelessWidget {
-  final String title;
+  final String? title;
   final String imageUrl;
-  final String subtitle;
+  final String? subtitle;
+  final VoidCallback? onTap;
+  final LinearGradient? gradient;
 
-  const CardWidget({required this.title, required this.imageUrl, required this.subtitle, super.key});
+  const CardWidget({this.title, required this.imageUrl, this.subtitle, this.onTap, this.gradient, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16),
-      child: Stack(
-        children: [
-          ImageAsset(
-            path: imageUrl,
-            fit: BoxFit.cover,
-            width: 140,
-            height: 230,
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              height: 230,
+    return InkWell(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Stack(
+          children: [
+            ImageAsset(
+              path: imageUrl,
+              fit: BoxFit.cover,
               width: 140,
-              decoration: const BoxDecoration(gradient: ThemeColors.gradientDark),
+              height: 230,
             ),
-          ),
-          Positioned(
-            width: 125,
-            bottom: 0,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 12, bottom: 12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    subtitle,
-                    style: TTypography.cardSubtitle.merge(TextStyle(
-                      color: ThemeColors.primaryWhite.withOpacity(0.75),
-                    )),
-                  ),
-                  const SizedBox(height: 2),
-                  Text(
-                    title,
-                    style: TTypography.cardTitle.merge(const TextStyle(
-                      color: ThemeColors.primaryWhite,
-                    )),
-                  ),
-                ],
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: 230,
+                width: 140,
+                decoration: BoxDecoration(gradient: gradient),
               ),
             ),
-          )
-        ],
+            Positioned(
+              width: 125,
+              bottom: 0,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12, bottom: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (subtitle != null)
+                      Text(
+                        subtitle!,
+                        style: TTypography.cardSubtitle.merge(TextStyle(
+                          color: ThemeColors.primaryWhite.withOpacity(0.75),
+                        )),
+                      ),
+                    const SizedBox(height: 2),
+                    if (title != null)
+                      Text(
+                        title!,
+                        style: TTypography.cardTitle.merge(const TextStyle(
+                          color: ThemeColors.primaryWhite,
+                        )),
+                      ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
