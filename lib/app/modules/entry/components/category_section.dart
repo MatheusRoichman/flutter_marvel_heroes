49014a85ee99d2abcac5042f2683319ea92883f0
Matheus Roichman <roichmankill@gmail.com>
2022-10-23 09:35:10 -0300

    fixed category section to work individually (each with its own loading and error handling)

--- lib/app/modules/entry/components/category_section.dart
+++ lib/app/modules/entry/components/category_section.dart
@@ -3,6 +3,8 @@ import 'package:flutter_marvel_heroes/app/core/components/card/card_list.dart';
 import 'package:flutter_marvel_heroes/app/core/components/card/card_widget.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../../core/components/buttons/primary_button.dart';
 import '../../../core/utils/constants.dart';
 import '../stores/characters_store.dart';
 import 'category_section_header.dart';
@@ -30,29 +32,11 @@ class _CategorySectionState extends State<CategorySection> {
     await store.getCharacters(category: widget.category);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Observer(builder: (context) {
-      if (store.characterState == CharacterState.done &&
-          store.characters[widget.category] != null &&
-          store.characters[widget.category]!.isEmpty) {
-        return Container();
-      }
+  Widget _buildCardList(String category) {
+    if (store.charactersStates[category] != CharacterState.done) return const SizedBox();
 
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CategorySectionHeader(title: widget.title, category: widget.category),
-          const SizedBox(height: 16),
-          if (store.characterState == CharacterState.loading)
-            const Center(
-                child: Padding(
-              padding: EdgeInsets.only(right: 24.0),
-              child: CircularProgressIndicator(),
-            )),
-          if (store.characterState == CharacterState.done)
-            CardList(
-                children: store.characters[widget.category]!.map((character) {
+    return store.characters[category] != null && store.characters[category]!.isNotEmpty ? CardList(
+                children: store.characters[category]!.map((character) {
               return CardWidget(
                 title: character.characterName,
                 subtitle: character.realName,
@@ -61,7 +45,40 @@ class _CategorySectionState extends State<CategorySection> {
                   Modular.to.pushNamed('/characters/${character.id}/');
                 },
               );
-            }).toList()),
+            }).toList()) : const Center(child: Text('Nenhum personagem encontrado'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String category = widget.category;
+    String title = widget.title;
+
+    return Observer(builder: (context) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CategorySectionHeader(title: title, category: category),
+          const SizedBox(height: 16),
+          if (store.charactersStates[category] == CharacterState.loading)
+            const Center(
+                child: Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: CircularProgressIndicator(),
+            )),
+          if (store.charactersStates[category] == CharacterState.error)
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    const Text('Erro ao carregar personagens'),
+                    const SizedBox(height: 10),
+                    PrimaryButton(text: 'Tentar novamente', onTap: _init),
+                  ],
+                ),
+              ),
+            ),
+          _buildCardList(category),
           const SizedBox(height: 40)
         ],
       );
