dcafe0633f4e2bf2bca9fea402770f36d0f8fba3
Matheus Roichman <roichmankill@gmail.com>
2022-11-02 18:14:28 -0300

    added CharacterRepository bind to EntryModule

--- lib/app/modules/entry/entry_module.dart
+++ lib/app/modules/entry/entry_module.dart
@@ -1,22 +1,20 @@
 import 'package:flutter_marvel_heroes/app/modules/character/character_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/repositories/character_repository.dart';
+import '../../core/stores/characters_store.dart';
 import 'pages/home_page.dart';
-import 'stores/characters_store.dart';
 
 class EntryModule extends Module {
   @override
   List<Bind> get binds => [
+        Bind.lazySingleton((i) => CharacterRepositoryImpl(i.get())),
         Bind.lazySingleton((i) => CharactersStore(i.get())),
       ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute(Modular.initialRoute, child: (_, args) => const HomePage()),
-        // ChildRoute('/characters/categories/:category_name',
-        //     child: (_, args) => CategoryPage(
-        //           category: args.params['category_name']!,
-        //         )),
-        ModuleRoute('/character/', module: CharacterModule(), transition: TransitionType.fadeIn),
+        ModuleRoute('/characters/', module: CharacterModule(), transition: TransitionType.fadeIn),
       ];
 }
