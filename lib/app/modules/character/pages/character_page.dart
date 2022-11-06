a21100bace87cc258bb4546d48433ab64ba431ad
Matheus Roichman <roichmankill@gmail.com>
2022-11-06 17:45:27 -0300

    switched ListView to SingleChildScrollView in pages

--- lib/app/modules/character/pages/character_page.dart
+++ lib/app/modules/character/pages/character_page.dart
@@ -48,104 +48,102 @@ class _CharacterPageState extends State<CharacterPage> {
         backgroundColor: ThemeColors.primaryBlack,
         extendBodyBehindAppBar: true,
         appBar: DarkTranslucidAppBar(),
-        body: ListView(
-          children: [
-            Observer(builder: (context) {
-              if (store.characterDetailsState == CharacterDetailsState.loading) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              if (store.characterDetailsState == CharacterDetailsState.error) {
-                return const Center(child: Text("Erro ao carregar os dados"));
-              }
-
-              return Stack(children: [
-                ImageAsset(
-                  path: '${Assets.images}/${store.character!.imageUrl}',
-                  width: screenSize.width,
-                  height: screenSize.height * 0.8,
-                  fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Observer(builder: (context) {
+            if (store.characterDetailsState == CharacterDetailsState.loading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+        
+            if (store.characterDetailsState == CharacterDetailsState.error) {
+              return const Center(child: Text("Erro ao carregar os dados"));
+            }
+        
+            return Stack(children: [
+              ImageAsset(
+                path: '${Assets.images}/${store.character!.imageUrl}',
+                width: screenSize.width,
+                height: screenSize.height * 0.8,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                height: screenSize.height * 0.8,
+                decoration: const BoxDecoration(
+                  gradient: ThemeColors.gradientBlack,
                 ),
-                Container(
-                  height: screenSize.height * 0.8,
-                  decoration: const BoxDecoration(
-                    gradient: ThemeColors.gradientBlack,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 24, right: 24, top: screenSize.height * 0.4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CharacterProfileHeader(
+                          realName: store.character!.realName,
+                          characterName: store.character!.characterName,
+                        ),
+                        const SizedBox(height: 48),
+                        CaracteristicsList(
+                          children: [
+                            CaracteristicCard(
+                                icon: const SvgAsset(
+                                  path: '${Assets.icons}/age.svg',
+                                  width: 10,
+                                  height: 20,
+                                  color: ThemeColors.primaryWhite,
+                                ),
+                                text: "${store.character!.age.toString()} anos"),
+                            CaracteristicCard(
+                                icon: const SvgAsset(
+                                  path: '${Assets.icons}/weight.svg',
+                                  width: 10,
+                                  height: 20,
+                                  color: ThemeColors.primaryWhite,
+                                ),
+                                text: "${store.character!.weight.toStringAsFixed(1)}kg"),
+                            CaracteristicCard(
+                                icon: const SvgAsset(
+                                  path: '${Assets.icons}/height.svg',
+                                  width: 10,
+                                  height: 20,
+                                  color: ThemeColors.primaryWhite,
+                                ),
+                                text: '${store.character!.height.toStringAsFixed(2)}m'),
+                            CaracteristicCard(
+                                icon: const SvgAsset(
+                                  path: '${Assets.icons}/universe.svg',
+                                  width: 10,
+                                  height: 20,
+                                  color: ThemeColors.primaryWhite,
+                                ),
+                                text: store.character!.homePlanet)
+                          ],
+                        ),
+                        const SizedBox(height: 24),
+                        Text(
+                          store.character!.description,
+                          style: TTypography.description.merge(const TextStyle(color: ThemeColors.primaryWhite)),
+                        ),
+                        CharacterAbilitiesSection(abilities: store.character!.abilities)
+                      ],
+                    ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 24, right: 24, top: screenSize.height * 0.4),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CharacterProfileHeader(
-                            realName: store.character!.realName,
-                            characterName: store.character!.characterName,
-                          ),
-                          const SizedBox(height: 48),
-                          CaracteristicsList(
-                            children: [
-                              CaracteristicCard(
-                                  icon: const SvgAsset(
-                                    path: '${Assets.icons}/age.svg',
-                                    width: 10,
-                                    height: 20,
-                                    color: ThemeColors.primaryWhite,
-                                  ),
-                                  text: "${store.character!.age.toString()} anos"),
-                              CaracteristicCard(
-                                  icon: const SvgAsset(
-                                    path: '${Assets.icons}/weight.svg',
-                                    width: 10,
-                                    height: 20,
-                                    color: ThemeColors.primaryWhite,
-                                  ),
-                                  text: "${store.character!.weight.toStringAsFixed(1)}kg"),
-                              CaracteristicCard(
-                                  icon: const SvgAsset(
-                                    path: '${Assets.icons}/height.svg',
-                                    width: 10,
-                                    height: 20,
-                                    color: ThemeColors.primaryWhite,
-                                  ),
-                                  text: '${store.character!.height.toStringAsFixed(2)}m'),
-                              CaracteristicCard(
-                                  icon: const SvgAsset(
-                                    path: '${Assets.icons}/universe.svg',
-                                    width: 10,
-                                    height: 20,
-                                    color: ThemeColors.primaryWhite,
-                                  ),
-                                  text: store.character!.homePlanet)
-                            ],
-                          ),
-                          const SizedBox(height: 24),
-                          Text(
-                            store.character!.description,
-                            style: TTypography.description.merge(const TextStyle(color: ThemeColors.primaryWhite)),
-                          ),
-                          CharacterAbilitiesSection(abilities: store.character!.abilities)
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 24, bottom: 48),
-                      child: CharacterFilmsSection(films: store.character!.films),
-                    ),
-                  ],
-                ),
-              ]);
-            }),
-          ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24, bottom: 48),
+                    child: CharacterFilmsSection(films: store.character!.films),
+                  ),
+                ],
+              ),
+            ]);
+          }),
         ));
   }
 
   @override
   void dispose() {
     super.dispose();
-    
+
     SystemChrome.setSystemUIOverlayStyle(SystemUICustomOverlayStyle.light);
   }
 }
--- lib/app/modules/character/pages/characters_category_page.dart
+++ lib/app/modules/character/pages/characters_category_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_marvel_heroes/app/core/components/card/card_grid.dart';
 import 'package:flutter_marvel_heroes/app/core/components/scaffold/main_scaffold.dart';
 import 'package:flutter_marvel_heroes/app/core/models/category_model.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -38,77 +39,72 @@ class _CharactersCategoryPageState extends State<CharactersCategoryPage> {
   Widget build(BuildContext context) {
     return MainScaffold(
       appBar: MainAppBar(),
-      body: ListView(
-        children: [
-          Observer(
-            builder: (_) {
-              if (store.charactersStates[widget.category.name] == CharacterState.loading) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              if (store.charactersStates[widget.category.name] == CharacterState.error) {
-                return Center(
-                  child: Column(
-                    children: [
-                      const Text('Erro ao carregar personagens'),
-                      const SizedBox(height: 10),
-                      PrimaryButton(text: 'Tentar novamente', onTap: _init),
-                    ],
-                  ),
-                );
-              }
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      body: SingleChildScrollView(
+        child: Observer(
+          builder: (_) {
+            if (store.charactersStates[widget.category.name] == CharacterState.loading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+      
+            if (store.charactersStates[widget.category.name] == CharacterState.error) {
+              return Center(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text(
-                      widget.category.displayName,
-                      style: TTypography.homeTitle,
-                    ),
-                    const SizedBox(height: 20),
-                    if (store.characters[widget.category.name]!.isEmpty)
-                      Center(
-                        child: Column(
-                          children: [
-                            const Text('Nenhum personagem encontrado'),
-                            const SizedBox(height: 10),
-                            PrimaryButton(text: 'Tentar novamente', onTap: _init),
-                          ],
-                        ),
-                      )
-                    else
-                      GridView.builder(
-                        shrinkWrap: true,
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 16,
-                          mainAxisSpacing: 16,
-                          mainAxisExtent: 230,
-                        ),
-                        itemCount: store.characters[widget.category.name]?.length,
-                        itemBuilder: (_, index) {
-                          Character character = store.characters[widget.category.name]![index];
-
-                          return CardWidget(
-                            title: character.characterName,
-                            subtitle: character.realName,
-                            imageUrl: '${Assets.images}/${character.imageUrl}',
-                            onTap: () {
-                              Modular.to.pushNamed('/characters/${character.id}/');
-                            },
-                            width: 170,
-                            height: 230,
-                          );
-                        },
-                      ),
+                    const Text('Erro ao carregar personagens'),
+                    const SizedBox(height: 10),
+                    PrimaryButton(text: 'Tentar novamente', onTap: _init),
                   ],
                 ),
               );
-            },
-          ),
-        ],
+            }
+      
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    widget.category.displayName,
+                    style: TTypography.homeTitle,
+                  ),
+                  const SizedBox(height: 20),
+                  if (store.characters[widget.category.name]!.isEmpty)
+                    Center(
+                      child: Column(
+                        children: [
+                          const Text('Nenhum personagem encontrado'),
+                          const SizedBox(height: 10),
+                          PrimaryButton(text: 'Tentar novamente', onTap: _init),
+                        ],
+                      ),
+                    )
+                  else
+                    CardGrid(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      mainAxisExtent: 230,
+                      itemCount: store.characters[widget.category.name]!.length,
+                      itemBuilder: (_, index) {
+                        Character character = store.characters[widget.category.name]![index];
+      
+                        return CardWidget(
+                          title: character.characterName,
+                          subtitle: character.realName,
+                          imageUrl: '${Assets.images}/${character.imageUrl}',
+                          onTap: () {
+                            Modular.to.pushNamed('/characters/${character.id}/');
+                          },
+                          width: 170,
+                          height: 230,
+                        );
+                      },
+                    ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
